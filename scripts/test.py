b837b483c993acd2c06c7b2597561b5320856470
objectbased <67526309+objectbased@users.noreply.github.com>
2023-09-16 17:45:58 -0400

    c
    c

--- scripts/test.py
+++ scripts/test.py
@@ -1,6 +1,7 @@
 import os
 import re
 import pandas as pd
+from tabulate import tabulate
 
 # Define the directory containing the .conf files
 conf_directory = "H:\\readme-tester\\syslog-ng\\conf.d\\integrations"
@@ -9,7 +10,7 @@ root = "H:\\readme-tester\\"
 github_repo_url = "https://github.com/objectbased/readme-tester/blob/main/syslog-ng/conf.d/integrations/"
 
 # Define the regex pattern to extract information from the conf files
-pattern = r'input\( source\((\w+)\) port\((\d+)\) protocol\("(\w+)"\)(?: protocol2\("(\w+)"\))?'
+pattern = r'input\( source\((\w+)\) port\((\w+)\) protocol\("(\w+)"\)(?: protocol2\("(\w+)"\))?'
 
 # Create an empty DataFrame to store the extracted data
 data = []
@@ -26,7 +27,7 @@ for filename in os.listdir(conf_directory):
                     protocol = f"{protocol},{protocol2}"
                 file_url = github_repo_url + filename
                 syslog_path = "/var/log/forward/%s/${HOST}/%s_${YEAR}-${MONTH}-${DAY}.log" % (source, source)
-                data.append((source, int(port), protocol, syslog_path, file_url))
+                data.append((source, port, protocol, syslog_path, file_url))
 
 # Create a DataFrame from the extracted data
 df = pd.DataFrame(data, columns=["source", "port", "protocol", "path", "origin"])
@@ -40,9 +41,13 @@ df.reset_index(drop=True, inplace=True)
 # Create a clickable link for the "origin" column
 df["origin"] = df["origin"].apply(lambda x: f'[Link]({x})')
 
+# Convert the DataFrame to a markdown table with left-aligned "port" column
+table = tabulate(df, headers='keys', tablefmt='pipe', stralign='left', numalign='left', showindex='never')
+
+
 # Update the README.md file with the extracted data
 with open(root + "README.md", 'w+') as readme_file:
-    readme_file.write("\n\n## Syslog-ng Port Tracker\n")
-    readme_file.write(df.to_markdown(index=False))
+    readme_file.write("## Syslog-ng Port Tracker\n")                      
+    readme_file.write(table)
 
 print("Data extraction and README update complete.")
